72ce0c674ec5b77f4413b337c31c7c184826ab85
João Pedro Lima <joaopedrocal90@gmail.com>
2026-05-17 14:11:25 -0300

    Refactor client and server code for distributed matrix multiplication tests

--- server.py
+++ server.py
@@ -18,27 +18,29 @@ def iniciar_servidor(porta):
     HOST = '127.0.0.1'
     
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, porta))
-        s.listen(1)
+        s.listen(5)
         print(f"Servidor aguardando conexão na porta {porta}...")
         
-        conn, addr = s.accept()
-        with conn:
-            print(f"Cliente conectado: {addr}")
-            
-            dados = receber_dado(conn)
-            sub_A = dados['sub_A']
-            B = dados['B']
-            print(f"Dados recebidos. Multiplicando {len(sub_A)} linhas...")
-            
-            resultado = multiplicar_submatriz(sub_A, B)
-            print("Cálculo concluído. Aguardando sinal de sincronismo do Cliente...")
-            
-            sinal = receber_dado(conn)
-            if sinal == "SINCRONISMO_OK":
-                print("Sinal recebido. Enviando resultado final...")
-                enviar_dado(conn, resultado)
-                print("Resultado enviado com sucesso!")
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print(f"Cliente conectado: {addr}")
+                
+                dados = receber_dado(conn)
+                sub_A = dados['sub_A']
+                B = dados['B']
+                print(f"Dados recebidos. Multiplicando {len(sub_A)} linhas...")
+                
+                resultado = multiplicar_submatriz(sub_A, B)
+                print("Cálculo concluído. Aguardando sinal de sincronismo do Cliente...")
+                
+                sinal = receber_dado(conn)
+                if sinal == "SINCRONISMO_OK":
+                    print("Sinal recebido. Enviando resultado final...")
+                    enviar_dado(conn, resultado)
+                    print("Resultado enviado com sucesso!")
 
 if __name__ == '__main__':
     porta_alvo = int(sys.argv[1]) if len(sys.argv) > 1 else 5001
